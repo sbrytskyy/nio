394c8f128fc3193683e0939c23421a54d8756aac
Serhiy Brytskyy <sbrytskyy@gmail.com>
2017-11-04 17:58:41 -0700

    Implemented separate socker acceptor

--- ServerPOC/src/main/java/com/sb/nio/poc/Server.java
+++ ServerPOC/src/main/java/com/sb/nio/poc/Server.java
@@ -1,29 +1,46 @@
 package com.sb.nio.poc;
 
 import java.io.IOException;
+import java.nio.channels.Selector;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Server {
 
+	private static final int MAX_INBOUND_CONNECTIONS = 1024;
+
 	private static final Logger log = LoggerFactory.getLogger(Server.class);
 
 	private int port;
+	private Queue<SocketContainer> inboundPortsQueue;
 	private ProtocolProcessor protocolProcessor;
+	
+	private BufferCache cache;
 	
 	public Server(int port) {
 		this.port = port;
+		inboundPortsQueue = new ArrayBlockingQueue<>(MAX_INBOUND_CONNECTIONS);
+		
+		cache = new BufferCache();
 	}
 
 	public void start() throws IOException {
 		log.info("Server configured with port: {}", port);
 
+		Selector selector = Selector.open();
+		
+		SocketAcceptor ssa = new SocketAcceptor(port, inboundPortsQueue, selector);
+		Thread accepterThread = new Thread(ssa);
+		accepterThread.start();
+		
 		protocolProcessor = new SimpleProcessor();
 		Thread pp = new Thread(protocolProcessor);
 		pp.start();
 		
-		SocketProcessor sp = new SocketProcessor(protocolProcessor, port);
+		SocketProcessor sp = new SocketProcessor(inboundPortsQueue, protocolProcessor, selector, cache);
 		Thread processorThread = new Thread(sp);
 		processorThread.start();
 	}
--- ServerPOC/src/main/java/com/sb/nio/poc/SocketProcessor.java
+++ ServerPOC/src/main/java/com/sb/nio/poc/SocketProcessor.java
@@ -1,12 +1,9 @@
 package com.sb.nio.poc;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Queue;
 import java.util.Set;
@@ -19,32 +16,24 @@ public class SocketProcessor implements Runnable {
 
 	private static final Logger log = LoggerFactory.getLogger(SocketProcessor.class);
 
+	private Queue<SocketContainer> inboundPortsQueue;
+	
 	private Queue<Message> outboundMessageQueue;
 
-	private ServerSocketChannel serverSocketChannel;
 	private Selector selector;
 
 	private ProtocolProcessor protocolProcessor;
 
 	private BufferCache cache;
 
-	public SocketProcessor(ProtocolProcessor protocolProcessor, int port) throws IOException {
-
+	public SocketProcessor(Queue<SocketContainer> inboundPortsQueue, ProtocolProcessor protocolProcessor, Selector selector, BufferCache cache) throws IOException {
+		this.inboundPortsQueue = inboundPortsQueue;
 		this.protocolProcessor = protocolProcessor;
-		this.cache = new BufferCache();
-
-		this.selector = Selector.open();
+		this.selector = selector;
+		this.cache = cache;
+		
 		outboundMessageQueue = new ConcurrentLinkedQueue<>();
 		protocolProcessor.init(outboundMessageQueue, selector);
-		
-		serverSocketInit(port);
-	}
-
-	private void serverSocketInit(int port) throws IOException {
-		serverSocketChannel = ServerSocketChannel.open();
-		serverSocketChannel.configureBlocking(false);
-		serverSocketChannel.bind(new InetSocketAddress(port));
-		serverSocketChannel.register(this.selector, SelectionKey.OP_ACCEPT);
 	}
 
 	@Override
@@ -52,28 +41,27 @@ public class SocketProcessor implements Runnable {
 		while (true) {
 			try {
 				int select = selector.select();
-
+								
 				if (select > 0) {
 					Set<SelectionKey> keys = selector.selectedKeys();
 
 					Iterator<SelectionKey> it = keys.iterator();
 					while (it.hasNext()) {
 						SelectionKey key = it.next();
-						it.remove();
+						
+						if (!key.isValid()) continue;
 
-						if (!key.isValid())
-							continue;
-
-						if (key.isAcceptable()) {
-							acceptSocket(key);
-						} else if (key.isReadable()) {
+						if (key.isReadable()) {
 							readFromSocket(key);
-						} else if (key.isWritable()) {
+						}
+						
+						if (key.isWritable()) {
 							writeToSocket(key);
 						}
 					}
 				}
-
+				
+				processSockets();
 				processMessages();
 
 			} catch (IOException e) {
@@ -82,26 +70,20 @@ public class SocketProcessor implements Runnable {
 		}
 	}
 
-	private void acceptSocket(SelectionKey key) throws IOException {
-		ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
-
-		SocketChannel socketChannel = serverSocketChannel.accept();
-		if (socketChannel != null) {
-			log.debug("Socket accepted: " + socketChannel);
-			socketChannel.configureBlocking(false);
-
-			SocketContainer sc = new SocketContainer(socketChannel);
-
-			SelectionKey keyRead = socketChannel.register(selector, SelectionKey.OP_READ);
-			keyRead.attach(sc);
-		}
-	}
-
 	private void processMessages() throws IOException {
 		while (!outboundMessageQueue.isEmpty()) {
 			Message message = outboundMessageQueue.poll();
 			SelectionKey key = message.getSc().getChannel().register(selector, SelectionKey.OP_WRITE);
 			key.attach(message);
+		}
+	}
+
+	private void processSockets() throws IOException {
+		while (!inboundPortsQueue.isEmpty()) {
+			SocketContainer sc = inboundPortsQueue.poll();
+			sc.getChannel().configureBlocking(false);
+			SelectionKey key = sc.getChannel().register(selector, SelectionKey.OP_READ);
+			key.attach(sc);
 		}
 	}
 
@@ -125,7 +107,7 @@ public class SocketProcessor implements Runnable {
 		ByteBuffer buffer = message.getBody();
 		int written = message.getSc().write(buffer);
 		log.debug("Outbound message to {}, written {} bytes.", message.getSc().getChannel(), written);
-
+		
 		cache.returnBuffer(buffer);
 
 		key.attach(null);
